83e31e222a6ae72e5e81cb0f7c78de12cdcb2f50
Thaís Faustino <thaisfsouza@outlook.com>
2022-09-20 21:41:16 -0300

    add JS
    adiciona formatação financeira, calculo de entrada, saída e total.

---
+++ scripts.js
@@ -0,0 +1,111 @@
+const Modal = {
+    open(){
+        // abrir modal
+        // adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.add('active')
+    },
+    close(){
+        // fechar o modal
+        // remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.remove('active')
+    }
+ }
+
+ const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -20000,
+        date: '23/10/2022',
+},
+    {
+        id: 2,
+        description: 'Criacao website',
+        amount: 500000,
+        date: '23/10/2022',
+},
+    {
+        id: 3,
+        description: 'Mercado',
+        amount: -10000,
+        date: '23/10/2022',
+}   
+]
+
+
+ const Transaction = {
+    incomes() {
+        let income = 0;
+        transactions.forEach(transaction => {
+            if(transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        })
+        return income;
+    },
+    expenses() {
+        let expense = 0;
+        transactions.forEach(transaction => {
+            if(transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        })
+        return expense;
+    },
+    total() {
+        
+        return Transaction.incomes() + Transaction.expenses();
+    }
+ }
+
+ const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td><img src="./assets/minus.svg" alt="Remover transação"></td>`
+
+            return html
+    },
+    updateBalance() {
+        document.getElementById('incomeDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document.getElementById('expenseDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document.getElementById('totalDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.total())
+    }
+ }
+
+ const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+        value = String(value).replace(/\D/g, "")
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL" 
+        })
+        return signal + value
+        }
+    
+ }
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
