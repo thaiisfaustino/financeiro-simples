c7fadf65aff66ec1f9fc3dd6fae3065e925251f4
Thaís Faustino <thaisfsouza@outlook.com>
2022-10-05 19:47:45 -0300

    Update javascript
    Adicionar e remover dados
    Tratamento dos valores em dinheiro
    Erro de preenchimento

--- scripts.js
+++ scripts.js
@@ -71,17 +71,18 @@ const Modal = {
     transactionsContainer: document.querySelector('#data-table tbody'),
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
         DOM.transactionsContainer.appendChild(tr)
     },
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
         const amount = Utils.formatCurrency(transaction.amount)
         const html = `
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
-            <td><img src="./assets/minus.svg" alt="Remover transação"></td>`
+            <td><img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação"></td>`
 
             return html
     },
@@ -99,6 +100,16 @@ const Modal = {
  }
 
  const Utils = {
+    formatAmount(value){
+        value = Number(value) * 100
+        console.log(value)
+
+        return value
+    },
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
         value = String(value).replace(/\D/g, "")
@@ -134,24 +145,44 @@ const Form = {
             throw new Error("Por favor, preencha todos os campos")
         }
     },
+    formatValues() {
+        let {description, amount, date} = Form.getValues()
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    clearFields() {
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+    
     submit(event) {
         event.preventDefault()
 
         try {
             Form.validadeFields()
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.close()
 
         } catch (error) {
             alert(error.message)
         }
 
-        Form.formatData()
         }
 }
 
 const App = {
     init() {
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance()
